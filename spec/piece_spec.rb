6de27fe7c268e46f9a10e279b64567c39e6fd4fc
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-10 10:44:00 +0000

    Add test for pieces being different types
    A test was added that checks pieces can be different types, eg pawns,
    knights etc. It is assumed that the Piece class will have a getter that
    returns a piece's type, and that the type is initially specified in the
    piece's initializer. This meant that other tests using the initialiser
    had to be updated to reflect the new number of arguments in the
    function.

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,12 +2,19 @@ require 'piece'
 
 describe Piece do
   it "can be black" do
-    piece = Piece.new("black")
+    piece = Piece.new("black", "pawn")
     expect(piece.colour).to eq("black")
   end
 
   it "can be white" do
-    piece = Piece.new("white")
+    piece = Piece.new("white", "pawn")
     expect(piece.colour).to eq("white")
+  end
+
+  it "is a certain type (pawn, knight, rook, queen etc)" do
+    piece = Piece.new("white", "pawn")
+    expect(piece.type).to eq("pawn")
+    piece = Piece.new("white", "queen")
+    expect(piece.type).to eq("queen")
   end
 end
