22961d359848e4ab397a054e1591da6ea820ad15
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-10 10:38:56 +0000

    Add test for pieces being white
    A test was added to check that pieces can be white. This test assumes
    that the colour is specified as part of the Piece class initializer,
    which meant that the black test also had to be updated.

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,7 +2,12 @@ require 'piece'
 
 describe Piece do
   it "can be black" do
-    piece = Piece.new
+    piece = Piece.new("black")
     expect(piece.colour).to eq("black")
+  end
+
+  it "can be white" do
+    piece = Piece.new("white")
+    expect(piece.colour).to eq("white")
   end
 end
