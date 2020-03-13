c420e7ad7ffe8964449328ff32c601b70217a6dd
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-13 16:27:46 +0000

    Add setup_new_game method to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,20 @@
 require_relative('./board.rb')
+require_relative('./piece.rb')
 
 class Game
   attr_reader :board
 
   def initialize
     @board = Board.new
+  end
+
+  def setup_new_game
+    piece_order = [Piece::ROOK, Piece::KNIGHT, Piece::BISHOP, Piece::QUEEN, Piece::KING, Piece::BISHOP, Piece::KNIGHT, Piece::ROOK]
+    (0..7).each do |column|
+      board.spaces[0][column] = Piece.new(Piece::WHITE, piece_order[column])
+      board.spaces[1][column] = Piece.new(Piece::WHITE, Piece::PAWN)
+      board.spaces[6][column] = Piece.new(Piece::BLACK, Piece::PAWN)
+      board.spaces[7][column] = Piece.new(Piece::BLACK, piece_order[column])
+    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,5 +6,29 @@ describe Game do
     it 'has a board that the game is played on' do
       expect(game.board).to be_instance_of(Board)
     end
+
+    it 'a new game can be setup on the game board' do
+      game.setup_new_game
+
+      piece_order = [Piece::ROOK, Piece::KNIGHT, Piece::BISHOP, Piece::QUEEN, Piece::KING, Piece::BISHOP, Piece::KNIGHT, Piece::ROOK]
+
+      (0..7).each do |column|
+        expect(game.board.spaces[0][column]).not_to eq(nil) 
+        expect(game.board.spaces[0][column].colour).to eq(Piece::WHITE)
+        expect(game.board.spaces[0][column].type).to eq(piece_order[column])
+
+        expect(game.board.spaces[1][column]).not_to eq(nil)
+        expect(game.board.spaces[1][column].colour).to eq(Piece::WHITE)
+        expect(game.board.spaces[1][column].type).to eq(Piece::PAWN)
+
+        expect(game.board.spaces[6][column]).not_to eq(nil)
+        expect(game.board.spaces[6][column].colour).to eq(Piece::BLACK)
+        expect(game.board.spaces[6][column].type).to eq(Piece::PAWN)
+
+        expect(game.board.spaces[7][column]).not_to eq(nil) 
+        expect(game.board.spaces[7][column].colour).to eq(Piece::BLACK)
+        expect(game.board.spaces[7][column].type).to eq(piece_order[column])
+      end
+    end
   end
 end
