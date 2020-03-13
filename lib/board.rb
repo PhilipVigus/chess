a10db4057813335c69b442ab5673c4fab8ffa86f
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-13 15:03:18 +0000

    Create add_piece method in Board class
    The class was created, but has no error handling at the moment.

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,11 @@
 class Board
   attr_reader :spaces
-  
+
   def initialize
     @spaces = Array.new(8) { Array.new(8) }
+  end
+
+  def add_piece(piece, row, column)
+    @spaces[row][column] = piece
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,23 @@
 require 'board'
+require 'piece'
 
 describe Board do
   context 'creation' do
     it 'can be created with a size of 8 x 8' do
       board = Board.new
+
       expect(board.spaces.size).to eq(8)
       expect(board.spaces[0].size).to eq(8)
+    end
+  end
+
+  context 'adding pieces' do
+    it 'you can add a piece to a square on the board' do
+      board = Board.new
+      piece = Piece.new("black", "pawn")
+      board.add_piece(piece, 0, 0)
+
+      expect(board.spaces[0][0]).to eq(piece)
     end
   end
 end
