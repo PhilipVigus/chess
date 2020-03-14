4e86e1718709cc2fb7dfbea12f55daf6894c1b07
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-14 08:47:53 +0000

    Refactor space_spec
    The space variable was made into an instance variable.

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -2,8 +2,8 @@ require 'space'
 
 describe Space do
   it 'is created by specifying a row and column' do
-    space = Space.new(1, 2)
-    expect(space.row).to eq(1)
-    expect(space.column).to eq(2)
+    @space = Space.new(1, 2)
+    expect(@space.row).to eq(1)
+    expect(@space.column).to eq(2)
   end
 end
