cd9dc3ada2e7d31cd1e23375fe4f8fc61310e485
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-14 08:44:57 +0000

    Refactor piece_spec
    Convert local to instance variables within individual tests.

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,20 +2,20 @@ require 'piece'
 
 describe Piece do
   it "can be black" do
-    piece = Piece.new(Piece::BLACK, Piece::PAWN)
-    expect(piece.colour).to eq(Piece::BLACK)
+    @piece = Piece.new(Piece::BLACK, Piece::PAWN)
+    expect(@piece.colour).to eq(Piece::BLACK)
   end
 
   it "can be white" do
-    piece = Piece.new(Piece::WHITE, Piece::PAWN)
-    expect(piece.colour).to eq(Piece::WHITE)
+    @piece = Piece.new(Piece::WHITE, Piece::PAWN)
+    expect(@piece.colour).to eq(Piece::WHITE)
   end
 
   it "is a certain type (pawn, knight, rook, queen etc)" do
-    piece = Piece.new(Piece::WHITE, Piece::PAWN)
-    expect(piece.type).to eq(Piece::PAWN)
+    @piece = Piece.new(Piece::WHITE, Piece::PAWN)
+    expect(@piece.type).to eq(Piece::PAWN)
     
-    piece = Piece.new(Piece::WHITE, Piece::QUEEN)
-    expect(piece.type).to eq(Piece::QUEEN)
+    @piece = Piece.new(Piece::WHITE, Piece::QUEEN)
+    expect(@piece.type).to eq(Piece::QUEEN)
   end
 end
