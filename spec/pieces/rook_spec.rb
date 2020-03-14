d6f4b3f216d2ca62856c1c5a6dcdcedf3fe2f865
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-14 09:21:24 +0000

    Check that rooks are a type of rook

--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -4,12 +4,18 @@ describe Rook do
   let(:white_rook) { Rook.new(Piece::WHITE) }
   let(:black_rook) { Rook.new(Piece::BLACK) }
 
-  it 'is a piece' do
-    expect(black_rook).to be_kind_of(Piece)
-  end
+  context 'creation' do
+    it 'is a piece' do
+      expect(black_rook).to be_kind_of(Piece)
+    end
 
-  it 'can be black or white' do
-    expect(black_rook.colour).to be(Piece::BLACK)
-    expect(white_rook.colour).to be(Piece::WHITE)
+    it 'can be black or white' do
+      expect(black_rook.colour).to be(Piece::BLACK)
+      expect(white_rook.colour).to be(Piece::WHITE)
+    end
+
+    it 'is a rook' do
+      expect(black_rook.type).to be(Piece::ROOK)
+    end
   end
 end
