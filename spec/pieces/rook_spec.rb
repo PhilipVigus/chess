7a7f46a50066673b19db601828ffab6318a92f77
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-14 09:19:24 +0000

    Check that rooks can be black or white

--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -1,8 +1,15 @@
 require_relative '../../lib/pieces/rook.rb'
 
 describe Rook do
+  let(:white_rook) { Rook.new(Piece::WHITE) }
+  let(:black_rook) { Rook.new(Piece::BLACK) }
+
   it 'is a piece' do
-    rook = Rook.new(Piece::BLACK)
-    expect(rook).to be_kind_of(Piece)
+    expect(black_rook).to be_kind_of(Piece)
+  end
+
+  it 'can be black or white' do
+    expect(black_rook.colour).to be(Piece::BLACK)
+    expect(white_rook.colour).to be(Piece::WHITE)
   end
 end
